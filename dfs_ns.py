c293c321dadbd9e3e36efd8fe77abff4fd58606e
Mavl <bmcmavl@gmail.com>
2019-12-01 02:22:44 +0300

    fix

--- dfs_ns.py
+++ dfs_ns.py
@@ -39,7 +39,7 @@ elif hosting == "aws-many" and "PRIVATE_PORT" in os.environ:
     PUBLIC_IP = urllib.request.urlopen("http://169.254.169.254/latest/meta-data/public-ipv4").read().decode("utf-8")
     PRIVATE_IP = urllib.request.urlopen("http://169.254.169.254/latest/meta-data/local-ipv4").read().decode("utf-8")
     PUBLIC_PORT = os.environ.get('PUBLIC_PORT', 23333)
-    PRIVATE_PORT = os.environ.get('PUBLIC_PORT', 33333)
+    PRIVATE_PORT = os.environ.get('PRIVATE_PORT', 33333)
     PUBLIC_ADDRESS = '{}:{}'.format(PUBLIC_IP, PUBLIC_PORT)
     PRIVATE_ADDRESS = '{}:{}'.format(PRIVATE_IP, PRIVATE_PORT)
 elif hosting == "custom" and "PUBLIC_ADDRESS" in os.environ and "PRIVATE_ADDRESS" in os.environ:
